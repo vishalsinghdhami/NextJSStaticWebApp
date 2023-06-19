73e4ee0e318e9045c899c6c9681ebb118b1b4d30
Vishal <singhdhamivishal@gmail.com>
2023-06-19 20:28:00 +0530

    program half layout

--- y-hub/app/Header/page.js
+++ y-hub/app/Header/page.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function Header() {
   return (
-    <main className='header'>
+    <main className='header d-inline-flex m-4 p-8'>
   
-  <ul >
+ 
       <li>
         <Link legacyBehavior href="/HomePage">
           <a>Home</a>
@@ -24,8 +25,7 @@ export default function Header() {
           <a>Contact</a>
         </Link>
       </li>
-    </ul>
-
+ 
     </main>
   )
 }
