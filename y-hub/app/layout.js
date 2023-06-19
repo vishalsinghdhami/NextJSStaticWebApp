0a1c13caccb813a7507f7f433f14f7116025e7c2
Vishal <singhdhamivishal@gmail.com>
2023-06-19 21:01:32 +0530

    navbar on all pages

--- y-hub/app/layout.js
+++ y-hub/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Header from './Header/page'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -10,8 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+    <Header></Header>
+    <main>{children}</main>
+    
+    </>
   )
 }
